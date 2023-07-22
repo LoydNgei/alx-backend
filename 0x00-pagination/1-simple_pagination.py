756ec3d08cde19338859c32683b9c9f44092ac40
Loyd Ngei <loydngei@gmail.com>
2023-07-22 13:37:27 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,7 @@ import math
 from typing import List
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> tuple:
     """ Return a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
         particular pagination parameters.
