a4d3d032167b6ab31f29e339c6fcd0adb10abd72
Loyd Ngei <loydngei@gmail.com>
2023-08-01 22:43:40 +0300

    Welcome to Holberton

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Render index.html"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    """ Main Function """
     app.run(debug=True)
