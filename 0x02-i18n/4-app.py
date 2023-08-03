f9be7b6c20a951c237406f773c5be312f0598e8b
Loyd Ngei <loydngei@gmail.com>
2023-08-03 11:46:42 +0300

    i18n

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """ Babel config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Render index.html"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(debug=True)
